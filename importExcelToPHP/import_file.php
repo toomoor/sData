bba06dd2f3ffeb58ea549384809a13414d474653
Amirhossein <46858785+toomoor@users.noreply.github.com>
2022-06-06 21:08:47 +0430

    import excel to php

---
+++ importExcelToPHP/import_file.php
@@ -0,0 +1,48 @@
+<?php
+#defind database info
+$host = "localhost";
+$username = "boy";
+$password = "12345";
+$database = "database";
+
+$con = new mysqli($host, $username, $password, $database);
+
+if ($con->connect_error) {
+    die("Connection failed: " . $con->connect_error);
+}
+// Database Structure 
+$sql = "CREATE TABLE IF NOT EXISTS names (
+    id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    fname VARCHAR(30) NOT NULL,
+    lname VARCHAR(30) NOT NULL)";
+if($con->query($sql) === TRUE){
+    #echo "Table created :)";
+}else{
+    echo "Error: " . $sql . "<br>" . $con->error;
+    die();
+}
+
+
+if(isset($_POST["submit_file"])){
+    $file = $_FILES["file"]["tmp_name"];
+    $file_open = fopen($file,"r");
+    while(! feof($file_open)){
+        $csv = fgetcsv($file_open);
+        
+        $fname = $csv[0];
+        $lname = $csv[1];
+
+        $sql = "INSERT INTO names (fname, lname) VALUES ('$fname','$lname')";
+        if (($fname == NULL && $lname != NULL) || $fname == TRUE){
+            if($con->query($sql) === TRUE){
+                #echo "insert into done.";
+            }else{
+                echo "Error: " . $sql . "<br>" . $con->error;
+                die();
+            }
+        }
+    }
+}
+fclose($file);
+header("location:view.php");
+?>
--- importExcelToPHP/view.php
+++ importExcelToPHP/view.php
@@ -1,14 +1,12 @@
-<?php include("database.php");
+<?php include("../database.php");
 
-$sql = "SELECT * FROM userdata WHERE `action` = '1'";
-$sql2 = "SELECT * FROM userdata WHERE `action` = '0'";
+$sql = "SELECT * FROM names";
 $result = $con->query($sql);
-$result2 = $con->query($sql2);
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <title>Welcome to Phonenote</title>
+  <title>Import CSV to php</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
@@ -19,23 +17,11 @@ $result2 = $con->query($sql2);
 <body>
   <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
     <ul class="navbar-nav">
-      <li class="nav-item active">
+      <li class="nav-item">
         <a class="nav-link disabled" href="#">List</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="insert.html">Add New</a>
-      </li>
-      <?php
-      if ($result2->num_rows > 0) {
-      ?>
-        <li class="nav-item">
-          <a class="nav-link" href="restore.php">Restore</a>
-        </li>
-      <?php
-      }
-      ?>
-      <li class="nav-item">
-        <a class="nav-link" href="importExcelToPHP/import.html">Import</a>
+        <a class="nav-link disabled" href="insert.html">Add New</a>
       </li>
     </ul>
   </nav>
@@ -47,7 +33,6 @@ $result2 = $con->query($sql2);
       <tr>
         <th>Firstname</th>
         <th>Lastname</th>
-        <th>Phone Number</th>
         <th></th>
       </tr>
     </thead>
@@ -59,10 +44,9 @@ $result2 = $con->query($sql2);
       <tr>
         <td><?= $row["fname"] ?></td>
         <td><?= $row["lname"] ?></td>
-        <td><?= $row["pnum"] ?></td>
         <td>
           <div class="btn-group btn-group-lg">
-            <a class="btn btn-danger" href="delete.php?id=<?= $row["id"] ?>">Delete</a>
+            <a class="btn btn-danger" href="detele.php?id=<?= $row["id"] ?>">Delete</a>
             <a class="btn btn-primary" href="update.php?id=<?= $row["id"] ?>">Update</a>
             <a class="btn btn-warning" href="view.php?id=<?= $row["id"] ?>">View</a>
           </div>
@@ -79,7 +63,8 @@ $result2 = $con->query($sql2);
   </div>
   <div class="row">
   <div class="col-sm-6">
-      <h4>Next feature : Import Databases.</h4>
+        <h4>New feature : Import CSV to php Databases.</h4>
+        <h4>Next feature : Import Databases.</h4>
     
     </div>
   </div>
